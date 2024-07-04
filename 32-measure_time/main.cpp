3d56e7c51bad669693bad3eeed25a800764eab6c
Yang Ye <leafyoung@gmail.com>
2024-07-04 13:24:53 +0000

    32-measure_time

---
+++ 32-measure_time/main.cpp
@@ -0,0 +1,24 @@
+#include <chrono>
+#include <iostream>
+#include <tuple>
+#include <vector>
+using namespace std;
+using namespace std::chrono;
+
+// Change below without the use of reference
+void test_value_copy(vector<int> &x) { x.size(); }
+
+int main() {
+  cout << "start" << endl;
+  vector<int> xx(1'000'000ull, 1);
+  auto start = high_resolution_clock::now();
+  for (int i = 0; i < 1'000ull; ++i) {
+    test_value_copy(xx);
+  }
+  auto end = high_resolution_clock::now();
+  cout << "end" << endl;
+  double time_taken = duration_cast<nanoseconds>(end - start).count() /
+                      static_cast<double>(1e9);
+
+  cout << time_taken << "\n";
+}
