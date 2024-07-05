dd9939d82068dfbfe32ff42d7ff66dada84e8fa0
Yang Ye <leafyoung@gmail.com>
2024-07-05 03:28:55 +0000

    73 update

--- 73-cache_locality/main.cpp
+++ 73-cache_locality/main.cpp
@@ -12,7 +12,7 @@ std::seed_seq seq{12123, 921312};
 std::mt19937 gen(seq);
 std::uniform_real_distribution<> dist(0.0, 10000.0);
 
-template <unsigned int N> auto gen_arr() {
+template <size_t N> auto gen_arr() {
   array<array<double, N>, N> A;
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
@@ -22,7 +22,7 @@ template <unsigned int N> auto gen_arr() {
   return A;
 }
 
-template <unsigned int N>
+template <size_t N>
 void mult_a_b(array<array<double, N>, N> A, array<array<double, N>, N> B,
               array<array<double, N>, N> C) {
   for (int i = 0; i < N; i++) {
@@ -35,7 +35,7 @@ void mult_a_b(array<array<double, N>, N> A, array<array<double, N>, N> B,
   }
 }
 
-template <unsigned int N>
+template <size_t N>
 void mult_a_bt(array<array<double, N>, N> A, array<array<double, N>, N> B,
                array<array<double, N>, N> C) {
 
@@ -49,30 +49,33 @@ void mult_a_bt(array<array<double, N>, N> A, array<array<double, N>, N> B,
   }
 }
 
+const size_t MATRIX_SIZE = 4096 * 2;
+const auto ITERATION = 10'000'000'000llu;
+
 int main() { // Test with 1024, 512
-  auto A{gen_arr<1024>()};
-  auto B{gen_arr<1024>()};
-  auto C{gen_arr<1024>()};
-  const auto iteration = 50'000'000'000llu;
+  auto A{gen_arr<MATRIX_SIZE>()};
+  auto B{gen_arr<MATRIX_SIZE>()};
+  auto C{gen_arr<MATRIX_SIZE>()};
+
   auto average = [](auto duration) {
-    return static_cast<float>(duration.count()) / static_cast<float>(iteration);
+    return static_cast<float>(duration.count()) / static_cast<float>(ITERATION);
   };
   for (int k = 0; k < 1; ++k) {
     {
       auto start = high_resolution_clock::now();
-      for (unsigned long long i = 0; i < iteration; ++i)
-        mult_a_b<1024>(A, B, C);
+      for (unsigned long long i = 0; i < ITERATION; ++i)
+        mult_a_b<MATRIX_SIZE>(A, B, C);
       auto stop = high_resolution_clock::now();
       auto duration = duration_cast<microseconds>(stop - start);
       cout << average(duration) << "\n";
     }
     {
       auto start = high_resolution_clock::now();
-      for (unsigned long long i = 0; i < iteration; ++i)
-        mult_a_bt<1024>(A, B, C);
+      for (unsigned long long i = 0; i < ITERATION; ++i)
+        mult_a_bt<MATRIX_SIZE>(A, B, C);
       auto stop = high_resolution_clock::now();
       auto duration = duration_cast<microseconds>(stop - start);
-      cout << average(duration) << " << \n";
+      cout << average(duration) << " << (faster) \n";
     }
   }
 }
