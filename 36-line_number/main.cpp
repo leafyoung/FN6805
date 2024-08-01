83db74f8a304e8e38bb9359fe619db9b3ceaf176
Yang Ye <leafyoung@gmail.com>
2024-08-01 12:07:43 +0000

    36 update

--- 36-line_number/main.cpp
+++ 36-line_number/main.cpp
@@ -6,12 +6,9 @@
 #include <vector>
 using namespace std;
 
-void line_number_static(int current, vector<int> &line_numbers) {
-  if (current > line_numbers.size()) {
-    line_numbers.resize(current);
-  }
-  for (int i = 0, line = 1; i < line_numbers.size(); ++i, ++line) {
-    line_numbers[i] = line;
+void line_number_static(int _current, vector<int> &line_numbers) {
+  for (size_t i = 0, j = 1; i < line_numbers.size(); ++i, ++j) {
+    line_numbers[i] = j;
   }
   return;
 
@@ -27,9 +24,6 @@ void line_number_static(int current, vector<int> &line_numbers) {
 
 void line_number_dynamic(int current, vector<int> &line_numbers) {
   const auto current_1 = current - 1;
-  if (current > line_numbers.size()) {
-    line_numbers.resize(current);
-  }
   for (int i = 0; i < line_numbers.size(); ++i) {
     if (i < current_1) {
       line_numbers[i] = current_1 - i;
@@ -44,9 +38,6 @@ void line_number_dynamic(int current, vector<int> &line_numbers) {
 void line_number_dynamic_opt(int current, vector<int> &line_numbers) {
   const auto current_1 = current - 1;
   const auto ln_size = line_numbers.size();
-  if (current > ln_size) {
-    line_numbers.resize(current);
-  }
   for (int i = 0; i < current_1; ++i) {
     line_numbers[i] = current_1 - i;
   }
@@ -66,18 +57,18 @@ void print_vector(vector<int> vs) {
 void test_line_number(string msg,
                       function<void(int, vector<int> &)> line_number_gen) {
   cout << msg << "\n";
-  auto x3 = vector<int>(30, 0);
-  auto test_cases = vector<int>{1, 10, 20, 30, 34};
-  for (int i = 0; i < test_cases.size(); ++i) {
-    line_number_gen(test_cases[i], x3);
-    print_vector(x3);
+  auto lines_numbers = vector<int>(17, 0);
+  auto current_test_cases = vector<int>{1, 2, 7, 12, 17};
+  for (int i = 0; i < current_test_cases.size(); ++i) {
+    cout << "Test: " << current_test_cases[i] << "\n";
+    line_number_gen(current_test_cases[i], lines_numbers);
+    print_vector(lines_numbers);
   }
+  cout << "\n";
 }
 
 int main() {
   test_line_number("line_number_static: "s, line_number_static);
-
   test_line_number("line_number_dynamic: "s, line_number_dynamic);
-
   test_line_number("line_number_dynamic_opt: "s, line_number_dynamic_opt);
 }
