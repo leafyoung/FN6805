2d4ad4ca7bfc8310acd8f83f0602f4c627592f90
Yang Ye <leafyoung@gmail.com>
2024-08-05 15:39:28 +0000

    update 54-fstream-text

--- 54-fstream_demo/main.cpp
+++ 54-fstream_demo/main.cpp
@@ -6,26 +6,10 @@ using namespace std;
 void test_write() {
   cout << "test_write()\n";
   ofstream out("54-fstream_demo/test.txt");
-  for (int i = 0; i < 10; ++i) {
-    out << "Hello World!";
-    out << i;
+  for (int i = 1; i < 11; ++i) {
+    out << "Hello World!" << i << "\n";
   }
   out.close();
-}
-
-void test_read_double() {
-  cout << "test_read_double()\n";
-  ifstream in("54-fstream_demo/input_double.txt");
-  vector<double> arr;
-  double tmp;
-  while (!in.eof()) {
-    in >> tmp;
-    arr.push_back(tmp);
-  }
-  in.close();
-  for (auto v : arr) {
-    cout << v << "\n";
-  }
 }
 
 void test_read() {
@@ -33,41 +17,53 @@ void test_read() {
   ifstream in("54-fstream_demo/test.txt");
   string sin;
   while (!in.eof()) {
+  // while (in >> sin) {
     in >> sin;
     cout << sin << "\n";
   }
   in.close();
 }
 
-void read_txt_overcome_error(bool overcome) {
-  cout << "read_txt_overcome_error(" << overcome << ")\n";
+void test_read_double() {
+  cout << "test_read_double()\n";
+  ifstream in("54-fstream_demo/input_double.txt");
+  vector<double> arr;
+  double tmp{0};
+  while (!in.eof()) {
+    in >> tmp;
+    arr.push_back(tmp);
+  }
+  in.close();
+  for (auto v : arr) {
+    cout << v << "\n";
+  }
+}
+
+void read_txt_overcome_error() {
+  cout << "read_txt_overcome_error()\n";
   ifstream in("54-fstream_demo/input_double_mix.txt");
   vector<double> arr;
-  double tmp{0}, sum{0};
+  double tmp{0};
   string dummy;
   while (!in.eof()) {
     in >> tmp;
-    if (overcome) {
-      if (in.fail()) {
-        in.clear();
-        in >> dummy;
-        continue;
-      }
+    if (in.fail()) { // if fail
+      in.clear(); // clear
+      in >> dummy; // read as string
+      continue; // read the next
     }
     arr.push_back(tmp);
   }
   in.close();
   for (auto v : arr) {
     cout << v << "\n";
-    sum += v;
   }
-  cout << "sum: " << sum << "\n";
 }
 
 void fout_with_convention() {
   cout << "fout_with_convention()\n";
-  ofstream out("54-fstream_demo/test.txt");
-  for (int i = 0; i < 10; ++i) {
+  ofstream out("54-fstream_demo/test2.txt");
+  for (int i = 1; i < 11; ++i) {
     out << "Hello World!\n";
     out << i << "\n";
   }
@@ -76,13 +72,13 @@ void fout_with_convention() {
 
 void fin_with_convension() {
   cout << "fin_with_convension()\n";
-  ifstream in("54-fstream_demo/test.txt");
+  ifstream in("54-fstream_demo/test2.txt");
   string sin, dummy;
   int iin;
   for (int i = 0; i < 10; ++i) {
-    getline(in, sin);
-    in >> iin;
-    getline(in, dummy);
+    getline(in, sin); // read one string one line
+    in >> iin; // read one int one line
+    getline(in, dummy); // consumes the rest of the line
     cout << sin << "\n" << iin << "\n";
   }
   in.close();
@@ -96,8 +92,7 @@ int main() {
   }
 
   {
-    read_txt_overcome_error(true);
-    // read_txt_overcome_error(false);
+    read_txt_overcome_error();
   }
 
   {
