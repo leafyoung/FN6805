e49b50f0427c2225a7d6e65262cb396ba2e5f8bb
Yang Ye <leafyoung@gmail.com>
2024-08-15 02:48:00 +0000

    30 update

--- 30-header-file/main.cpp
+++ 30-header-file/main.cpp
@@ -3,8 +3,12 @@
 #include <iostream>
 using namespace std;
 
-#include "a_func.h"
-#include "b_func.h"
-#include "c_func.cpp"
+// #include "a_func.h"
+// #include "b_func.h"
 
-int main() { cout << a_func() + b_func() << "\n"; }
+void test_overload();
+
+int main() {
+  test_overload();
+  // cout << "a_func() + b_func(): " << a_func() + b_func() << "\n";
+}
---
+++ 30-header-file/test_overload.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+
+void print(double x) { cout << "double:" << x << "\n"; };
+void print(float x) { cout << "float:" << x << "\n"; };
+void print(int x) { cout << "int:" << x << "\n"; };
+void print(int x, bool b = false) { cout << "int and bool:" << x << "\n"; };
+
+void test_overload() {
+  cout << "test_overload():\n";
+  print(1.0);     // good, call double
+  print(1.0f);    // good, call float
+  print(1);       // error
+  print(1, true); // good
+  cout << "\n";
+}
