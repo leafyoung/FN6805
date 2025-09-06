875bde212e0858244aba221004c3e78390124227
Yang Ye <leafyoung@gmail.com>
2025-09-06 12:59:48 +0000

    add sample test

---
+++ 62-rand_dist/sample_test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+// input data
+struct Param {
+  int a;
+  double b;
+};
+
+// output: we can return one or multiple
+struct Result {
+  int r;
+};
+
+// process
+Result produce(Param p) { return {0}; }
+
+// test
+void test_produce() {
+  produce({.a = 0, .b = 3.0}).r == 0 ? true : throw logic_error("wrong result");
+  // Or any problem-specific test
+}
+
+int q3() {
+  test_produce();
+  return 0;
+}
