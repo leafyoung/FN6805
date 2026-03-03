2c43ae9d8c309f39d7367f55571ca194f8292714
Yang Ye <leafyoung@gmail.com>
2026-03-03 23:41:44 +0800

    36 update

--- 36-line_number/more_with_stl.cpp
+++ 36-line_number/more_with_stl.cpp
@@ -7,13 +7,11 @@ using namespace std;
 void line_number_dynamic_opt1(int current, vector<int> &line_numbers) {
   // optimized
   const auto current_1 = current - 1;
-  if (current > line_numbers.size()) {
+  if (current > static_cast<int>(line_numbers.size())) {
     line_numbers.resize(current);
-  } else {
-    line_numbers.clear();
   }
-  if (current < line_numbers.size() / 2) {
-    for (int i = 0; i < line_numbers.size(); ++i) {
+  if (current < static_cast<int>(line_numbers.size()) / 2) {
+    for (int i = 0; i < static_cast<int>(line_numbers.size()); ++i) {
       if (i > current_1) {
         line_numbers[i] = i - current_1;
       } else if (i < current_1) {
@@ -27,11 +25,11 @@ void line_number_dynamic_opt1(int current, vector<int> &line_numbers) {
       }
     }
   } else {
-    for (int i = 0; i < line_numbers.size(); ++i) {
+    for (int i = 0; i < static_cast<int>(line_numbers.size()); ++i) {
       if (i < current_1) {
         line_numbers[i] = current_1 - i;
       } else if (i > current_1) {
-        line_numbers[i] = i - current;
+        line_numbers[i] = i - current_1;
       } else if (i == current_1) { // least likely
         line_numbers[i] = current;
       } else {
@@ -47,7 +45,7 @@ void line_number_dynamic_opt2(int current, vector<int> &line_numbers) {
   if (current < 1) {
     throw logic_error("current cannot be < 1");
   }
-  if (current > line_numbers.size()) {
+  if (current > static_cast<int>(line_numbers.size())) {
     line_numbers.resize(current);
   }
   const auto current_1 = current - 1;
@@ -55,7 +53,7 @@ void line_number_dynamic_opt2(int current, vector<int> &line_numbers) {
   generate(line_numbers.begin(), line_numbers.begin() + current - 1,
            [current_1, &i]() { return current_1 - i++; });
   line_numbers[current_1] = current;
-  ++i;
+  i = 1;
   generate(line_numbers.begin() + current, line_numbers.end(),
-           [current_1, &i]() { return i++ - current_1; });
+           [&i]() { return i++; });
 }
