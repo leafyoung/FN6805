fe5de767b7283d507fedd78cafb52c195387dd1c
Yang Ye <yy@runchee.com>
2026-08-26 03:11:36 +0000

    11-voucher

---
+++ 11-voucher/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Voucher {
+  int value;
+  bool is_spent = false;
+};
+
+struct Household {
+  long long id;
+  string address;
+};
+
+using Household_ID = long long;
+
+struct HouseholdVouchers {
+  Household_ID id;
+  vector<Voucher> vouchers;
+  int balance;
+};
+
+struct HouseholdVouchersEx {
+  Household_ID id;
+  vector<Voucher> vouchers;
+  int balance;
+  bool issued;
+  int issued_time;
+  int issued_amount;
+};
+
+int main() {
+  std::cout << boolalpha;
+
+  Voucher v1{100}, v2{40};
+  std::cout << v1.value << endl;
+  std::cout << v2.value << endl;
+
+  Household h{1234, "Jurong East"};
+
+  HouseholdVouchers hv{1234, {{3}, {4}}, 3};
+  std::cout << hv.vouchers.back().value << endl;
+
+  Voucher v3(v1);
+
+  vector<int> a{1, 2, 3}, b{4, 5, 6};
+
+  vector<vector<int>> aInt5 = {
+      {11, 12, 13, 14}, {21, 22, 23, 24}, {31, 32, 33, 34},
+      {41, 42, 43, 44}, {51, 52, 53, 54},
+  };
+  cout << aInt5[3][3] << endl;
+}
