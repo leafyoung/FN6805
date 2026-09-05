06fa432b405d8ff998e1debc75f75106c8b590a3
Yang Ye <leafyoung@gmail.com>
2026-09-05 22:21:02 +0800

    54 improve

--- 54-fstream_demo/main.cpp
+++ 54-fstream_demo/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void test_write() {
   cout << "test_write()\n";
-  ofstream out("54-fstream_demo/test.txt");
+  ofstream out("./test.txt");
   for (int i = 1; i < 11; ++i) {
     out << "Hello World!" << i << '\n';
   }
@@ -14,7 +14,7 @@ void test_write() {
 
 void test_read() {
   cout << "test_read()\n";
-  ifstream in("54-fstream_demo/test.txt");
+  ifstream in("./test.txt");
   string sin;
   while (in >> sin) {
     cout << sin << '\n';
@@ -24,7 +24,7 @@ void test_read() {
 
 void test_read_double() {
   cout << "test_read_double()\n";
-  ifstream in("54-fstream_demo/input_double.txt");
+  ifstream in("./input_double.txt");
   vector<double> arr;
   double tmp{0};
   while (in >> tmp) {
@@ -38,7 +38,7 @@ void test_read_double() {
 
 void read_txt_overcome_error() {
   cout << "read_txt_overcome_error()\n";
-  ifstream in("54-fstream_demo/input_double_mix.txt");
+  ifstream in("./input_double_mix.txt");
   vector<double> arr;
   double tmp{0};
   string dummy;
@@ -60,7 +60,7 @@ void read_txt_overcome_error() {
 
 void write_with_convention() {
   cout << "write_with_convention()\n";
-  ofstream out("54-fstream_demo/test2.txt");
+  ofstream out("./test2.txt");
   for (int i = 1; i < 11; ++i) {
     out << "Hello World!\n";
     out << i << '\n';
@@ -70,13 +70,15 @@ void write_with_convention() {
 
 void read_with_convension() {
   cout << "read_with_convension()\n";
-  ifstream in("54-fstream_demo/test2.txt");
+  ifstream in("./test2.txt");
   string sin, dummy;
   int iin;
   for (int i = 0; i < 10; ++i) {
     getline(in, sin);   // read one string one line
     in >> iin;          // read one int one line
     getline(in, dummy); // consumes the rest of the line
+    // dummy is actually empty, but we need it to finish the reading of the line
+    cout << "dummy: " << (dummy == "" ? "Yes" : "No") << '\n';
     cout << sin << '\n' << iin << '\n';
   }
   in.close();
@@ -89,7 +91,9 @@ int main() {
     test_read_double();
   }
 
-  { read_txt_overcome_error(); }
+  {
+    read_txt_overcome_error();
+  }
 
   {
     write_with_convention();
